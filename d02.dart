6ed1175286f60ad17dcee79698003be3c392769d
xyr <cy56@ggook.com>
2022-05-29 18:42:09 +0800

    d02

---
+++ d02.dart
@@ -0,0 +1,40 @@
+// main() {
+//   print('你好 dart');
+// }
+
+void main() {
+  // var str = '你好 dart2';
+  // var myNum = 1234;
+  // print(str);
+  // print(myNum);
+
+  // //字符串
+  // String str2 = '你好 dart3';
+  // print(str2);
+
+  // //数字类型
+  // int num = 12345;
+  // print(num);
+
+  // var str3 = '';
+  // str3 = 123;
+  // print(str3);
+
+  // int myNum = 2134;
+  // print(myNum);
+
+  //const 常量
+  // const PI = 3.14159;
+  // PI = 13.14159; //错误的写法 常量不可以修改
+  // print(PI);
+
+  // final 常量2
+  // final PI = 3.14159;
+  // PI = 3.1;
+  // print(PI);
+
+  final a = new DateTime.now();
+  print(a);
+
+  // const b = new DateTime.now();
+}
